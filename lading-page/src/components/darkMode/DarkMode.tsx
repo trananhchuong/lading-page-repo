ece11724104e055e33beb2b0a186eb19f1b4323d
Chuong Tran <39957959+trananhchuong@users.noreply.github.com>
2021-03-06 16:46:16 +0700

    update style dark mode

--- lading-page/src/components/darkMode/DarkMode.tsx
+++ lading-page/src/components/darkMode/DarkMode.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Switch } from 'antd';
+import { CloseOutlined, CheckOutlined } from '@ant-design/icons';
+import { IMG_DARDMODE } from '../../constant/ImagesConstant';
 
 DarkMode.propTypes = {
 
 };
 
 function DarkMode(props) {
-
-
     const clickedClass = 'clicked';
     const body = document.body;
     const lightTheme = 'light';
     const darkTheme = 'dark';
+    const NightIcon = IMG_DARDMODE.NightIcon;
+    const SunnyIcon = IMG_DARDMODE.SunnyIcon;
+
+
     let theme;
 
     if (localStorage) {
@@ -27,24 +32,35 @@ function DarkMode(props) {
 
         if (theme === darkTheme) {
             body.classList.replace(darkTheme, lightTheme);
-            e.target.classList.remove(clickedClass);
+            // e.target.classList.remove(clickedClass);
             localStorage.setItem('theme', 'light');
             theme = lightTheme;
         } else {
             body.classList.replace(lightTheme, darkTheme);
-            e.target.classList.add(clickedClass);
+            // e.target.classList.add(clickedClass);
             localStorage.setItem('theme', 'dark');
             theme = darkTheme;
         }
     };
 
-    return (
-        <button
-            className={theme === 'dark' ? clickedClass : ''}
+
+    // <button
+    //     className={theme === 'dark' ? clickedClass : ''}
+    //     id="darkMode"
+    //     onClick={e => switchTheme(e)}
+    // >set</button>
+
+    return <>
+        <Switch
             id="darkMode"
             onClick={e => switchTheme(e)}
-        >set</button>
-    );
+            className={theme === 'dark' ? clickedClass : ''}
+            checkedChildren={<NightIcon />}
+            unCheckedChildren={<SunnyIcon />}
+            defaultChecked
+        />
+    </>
+        ;
 }
 
 export default DarkMode;
--- lading-page/src/constant/ImagesConstant.ts
+++ lading-page/src/constant/ImagesConstant.ts
@@ -5,6 +5,12 @@ import BackgroundImg from '../assets/img/background.jpg';
 import DataNone from '../assets/img/icon-search-none.svg';
 import SearchIcon from '../assets/img/search-icon.svg';
 
+
+import { ReactComponent as NightIcon } from '../assets/img/night-icon.svg';
+import { ReactComponent as SunnyIcon } from '../assets/img/sunny-icon.svg';
+
+
+
 export const IMAGES_HEADER = {
     CloseMenu,
     MenuIcon,
@@ -16,5 +22,12 @@ export const BACKGROUND_CONSTANT = {
     DataNone,
     SearchIcon
 };
+
+
+export const IMG_DARDMODE = {
+    NightIcon,
+    SunnyIcon
+};
+
 
 
